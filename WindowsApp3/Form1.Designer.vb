60030b7cbff0f981afe95a527100d26529bfaeb7
Diego Custodio <dk-lokahi@hotmail.com>
2019-04-05 02:55:41 -0300

    Arquivos importados!

---
+++ WindowsApp3/Form1.Designer.vb
@@ -0,0 +1,300 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSete = New System.Windows.Forms.Button()
+        Me.txtPainel = New System.Windows.Forms.TextBox()
+        Me.btnOito = New System.Windows.Forms.Button()
+        Me.btnNove = New System.Windows.Forms.Button()
+        Me.btnSeis = New System.Windows.Forms.Button()
+        Me.btnCinco = New System.Windows.Forms.Button()
+        Me.btnQuatro = New System.Windows.Forms.Button()
+        Me.btnTres = New System.Windows.Forms.Button()
+        Me.btnDois = New System.Windows.Forms.Button()
+        Me.btnUm = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.btnZero = New System.Windows.Forms.Button()
+        Me.btnVirgula = New System.Windows.Forms.Button()
+        Me.btnDividir = New System.Windows.Forms.Button()
+        Me.btnMultiplicar = New System.Windows.Forms.Button()
+        Me.btnSubtrair = New System.Windows.Forms.Button()
+        Me.btnAdicao = New System.Windows.Forms.Button()
+        Me.btnLimpar = New System.Windows.Forms.Button()
+        Me.btnPorcentagem = New System.Windows.Forms.Button()
+        Me.btnBackspace = New System.Windows.Forms.Button()
+        Me.btnResultado = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnSete
+        '
+        Me.btnSete.Location = New System.Drawing.Point(204, 128)
+        Me.btnSete.Name = "btnSete"
+        Me.btnSete.Size = New System.Drawing.Size(90, 29)
+        Me.btnSete.TabIndex = 0
+        Me.btnSete.Text = "7"
+        Me.btnSete.UseVisualStyleBackColor = True
+        '
+        'txtPainel
+        '
+        Me.txtPainel.Location = New System.Drawing.Point(14, 35)
+        Me.txtPainel.Name = "txtPainel"
+        Me.txtPainel.Size = New System.Drawing.Size(332, 20)
+        Me.txtPainel.TabIndex = 1
+        '
+        'btnOito
+        '
+        Me.btnOito.Location = New System.Drawing.Point(108, 128)
+        Me.btnOito.Name = "btnOito"
+        Me.btnOito.Size = New System.Drawing.Size(90, 29)
+        Me.btnOito.TabIndex = 2
+        Me.btnOito.Text = "8"
+        Me.btnOito.UseVisualStyleBackColor = True
+        '
+        'btnNove
+        '
+        Me.btnNove.Location = New System.Drawing.Point(12, 128)
+        Me.btnNove.Name = "btnNove"
+        Me.btnNove.Size = New System.Drawing.Size(90, 29)
+        Me.btnNove.TabIndex = 3
+        Me.btnNove.Text = "9"
+        Me.btnNove.UseVisualStyleBackColor = True
+        '
+        'btnSeis
+        '
+        Me.btnSeis.Location = New System.Drawing.Point(12, 163)
+        Me.btnSeis.Name = "btnSeis"
+        Me.btnSeis.Size = New System.Drawing.Size(90, 29)
+        Me.btnSeis.TabIndex = 4
+        Me.btnSeis.Text = "6"
+        Me.btnSeis.UseVisualStyleBackColor = True
+        '
+        'btnCinco
+        '
+        Me.btnCinco.Location = New System.Drawing.Point(108, 163)
+        Me.btnCinco.Name = "btnCinco"
+        Me.btnCinco.Size = New System.Drawing.Size(90, 29)
+        Me.btnCinco.TabIndex = 5
+        Me.btnCinco.Text = "5"
+        Me.btnCinco.UseVisualStyleBackColor = True
+        '
+        'btnQuatro
+        '
+        Me.btnQuatro.Location = New System.Drawing.Point(204, 163)
+        Me.btnQuatro.Name = "btnQuatro"
+        Me.btnQuatro.Size = New System.Drawing.Size(90, 29)
+        Me.btnQuatro.TabIndex = 6
+        Me.btnQuatro.Text = "4"
+        Me.btnQuatro.UseVisualStyleBackColor = True
+        '
+        'btnTres
+        '
+        Me.btnTres.Location = New System.Drawing.Point(12, 198)
+        Me.btnTres.Name = "btnTres"
+        Me.btnTres.Size = New System.Drawing.Size(90, 29)
+        Me.btnTres.TabIndex = 7
+        Me.btnTres.Text = "3"
+        Me.btnTres.UseVisualStyleBackColor = True
+        '
+        'btnDois
+        '
+        Me.btnDois.Location = New System.Drawing.Point(108, 198)
+        Me.btnDois.Name = "btnDois"
+        Me.btnDois.Size = New System.Drawing.Size(90, 29)
+        Me.btnDois.TabIndex = 8
+        Me.btnDois.Text = "2"
+        Me.btnDois.UseVisualStyleBackColor = True
+        '
+        'btnUm
+        '
+        Me.btnUm.Location = New System.Drawing.Point(204, 198)
+        Me.btnUm.Name = "btnUm"
+        Me.btnUm.Size = New System.Drawing.Size(90, 29)
+        Me.btnUm.TabIndex = 9
+        Me.btnUm.Text = "1"
+        Me.btnUm.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(12, 233)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(90, 29)
+        Me.Button10.TabIndex = 10
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'btnZero
+        '
+        Me.btnZero.Location = New System.Drawing.Point(108, 233)
+        Me.btnZero.Name = "btnZero"
+        Me.btnZero.Size = New System.Drawing.Size(90, 29)
+        Me.btnZero.TabIndex = 11
+        Me.btnZero.Text = "0"
+        Me.btnZero.UseVisualStyleBackColor = True
+        '
+        'btnVirgula
+        '
+        Me.btnVirgula.Location = New System.Drawing.Point(204, 233)
+        Me.btnVirgula.Name = "btnVirgula"
+        Me.btnVirgula.Size = New System.Drawing.Size(90, 29)
+        Me.btnVirgula.TabIndex = 12
+        Me.btnVirgula.Text = ","
+        Me.btnVirgula.UseVisualStyleBackColor = True
+        '
+        'btnDividir
+        '
+        Me.btnDividir.Location = New System.Drawing.Point(300, 93)
+        Me.btnDividir.Name = "btnDividir"
+        Me.btnDividir.Size = New System.Drawing.Size(46, 29)
+        Me.btnDividir.TabIndex = 13
+        Me.btnDividir.Text = "/"
+        Me.btnDividir.UseVisualStyleBackColor = True
+        '
+        'btnMultiplicar
+        '
+        Me.btnMultiplicar.Location = New System.Drawing.Point(300, 128)
+        Me.btnMultiplicar.Name = "btnMultiplicar"
+        Me.btnMultiplicar.Size = New System.Drawing.Size(46, 29)
+        Me.btnMultiplicar.TabIndex = 14
+        Me.btnMultiplicar.Text = "x"
+        Me.btnMultiplicar.UseVisualStyleBackColor = True
+        '
+        'btnSubtrair
+        '
+        Me.btnSubtrair.Location = New System.Drawing.Point(300, 163)
+        Me.btnSubtrair.Name = "btnSubtrair"
+        Me.btnSubtrair.Size = New System.Drawing.Size(46, 29)
+        Me.btnSubtrair.TabIndex = 15
+        Me.btnSubtrair.Text = "-"
+        Me.btnSubtrair.UseVisualStyleBackColor = True
+        '
+        'btnAdicao
+        '
+        Me.btnAdicao.Location = New System.Drawing.Point(300, 198)
+        Me.btnAdicao.Name = "btnAdicao"
+        Me.btnAdicao.Size = New System.Drawing.Size(46, 29)
+        Me.btnAdicao.TabIndex = 16
+        Me.btnAdicao.Text = "+"
+        Me.btnAdicao.UseVisualStyleBackColor = True
+        '
+        'btnLimpar
+        '
+        Me.btnLimpar.Location = New System.Drawing.Point(12, 93)
+        Me.btnLimpar.Name = "btnLimpar"
+        Me.btnLimpar.Size = New System.Drawing.Size(90, 29)
+        Me.btnLimpar.TabIndex = 17
+        Me.btnLimpar.Text = "AC"
+        Me.btnLimpar.UseVisualStyleBackColor = True
+        '
+        'btnPorcentagem
+        '
+        Me.btnPorcentagem.Location = New System.Drawing.Point(204, 93)
+        Me.btnPorcentagem.Name = "btnPorcentagem"
+        Me.btnPorcentagem.Size = New System.Drawing.Size(90, 29)
+        Me.btnPorcentagem.TabIndex = 18
+        Me.btnPorcentagem.Text = "%"
+        Me.btnPorcentagem.UseVisualStyleBackColor = True
+        '
+        'btnBackspace
+        '
+        Me.btnBackspace.Location = New System.Drawing.Point(108, 93)
+        Me.btnBackspace.Name = "btnBackspace"
+        Me.btnBackspace.Size = New System.Drawing.Size(90, 29)
+        Me.btnBackspace.TabIndex = 19
+        Me.btnBackspace.Text = "<="
+        Me.btnBackspace.UseVisualStyleBackColor = True
+        '
+        'btnResultado
+        '
+        Me.btnResultado.Location = New System.Drawing.Point(300, 233)
+        Me.btnResultado.Name = "btnResultado"
+        Me.btnResultado.Size = New System.Drawing.Size(46, 29)
+        Me.btnResultado.TabIndex = 20
+        Me.btnResultado.Text = "="
+        Me.btnResultado.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(309, 320)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 13)
+        Me.Label1.TabIndex = 21
+        Me.Label1.Text = "by Diego"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(360, 342)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnResultado)
+        Me.Controls.Add(Me.btnBackspace)
+        Me.Controls.Add(Me.btnPorcentagem)
+        Me.Controls.Add(Me.btnLimpar)
+        Me.Controls.Add(Me.btnAdicao)
+        Me.Controls.Add(Me.btnSubtrair)
+        Me.Controls.Add(Me.btnMultiplicar)
+        Me.Controls.Add(Me.btnDividir)
+        Me.Controls.Add(Me.btnVirgula)
+        Me.Controls.Add(Me.btnZero)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.btnUm)
+        Me.Controls.Add(Me.btnDois)
+        Me.Controls.Add(Me.btnTres)
+        Me.Controls.Add(Me.btnQuatro)
+        Me.Controls.Add(Me.btnCinco)
+        Me.Controls.Add(Me.btnSeis)
+        Me.Controls.Add(Me.btnNove)
+        Me.Controls.Add(Me.btnOito)
+        Me.Controls.Add(Me.txtPainel)
+        Me.Controls.Add(Me.btnSete)
+        Me.Name = "Form1"
+        Me.Text = "Calculadora"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnSete As Button
+    Friend WithEvents txtPainel As TextBox
+    Friend WithEvents btnOito As Button
+    Friend WithEvents btnNove As Button
+    Friend WithEvents btnSeis As Button
+    Friend WithEvents btnCinco As Button
+    Friend WithEvents btnQuatro As Button
+    Friend WithEvents btnTres As Button
+    Friend WithEvents btnDois As Button
+    Friend WithEvents btnUm As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents btnZero As Button
+    Friend WithEvents btnVirgula As Button
+    Friend WithEvents btnDividir As Button
+    Friend WithEvents btnMultiplicar As Button
+    Friend WithEvents btnSubtrair As Button
+    Friend WithEvents btnAdicao As Button
+    Friend WithEvents btnLimpar As Button
+    Friend WithEvents btnPorcentagem As Button
+    Friend WithEvents btnBackspace As Button
+    Friend WithEvents btnResultado As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ WindowsApp3/Form1.vb
@@ -0,0 +1,119 @@
+﻿Public Class Form1
+    Dim operador As String
+    Dim num1 As Double
+    Dim num2 As Double
+
+    Private Sub TxtPainel_TextChanged(sender As Object, e As EventArgs) Handles txtPainel.TextChanged
+
+    End Sub
+
+    Private Sub BtnAdicao_Click(sender As Object, e As EventArgs) Handles btnAdicao.Click
+        If txtPainel.Text = "" Then
+            MessageBox.Show("Ação inválida, insira um número!")
+        Else
+            num1 = txtPainel.Text
+            operador = "+"
+            txtPainel.Text = ""
+        End If
+    End Sub
+    Private Sub BtnSubtrair_Click(sender As Object, e As EventArgs) Handles btnSubtrair.Click
+        If txtPainel.Text = "" Then
+            MessageBox.Show("Ação inválida, insira um número!")
+        Else
+            num1 = txtPainel.Text
+            operador = "-"
+            txtPainel.Text = ""
+        End If
+    End Sub
+    Private Sub BtnMultiplicar_Click(sender As Object, e As EventArgs) Handles btnMultiplicar.Click
+        If txtPainel.Text = "" Then
+            MessageBox.Show("Ação inválida, insira um número!")
+        Else
+            num1 = txtPainel.Text
+            operador = "x"
+            txtPainel.Text = ""
+        End If
+    End Sub
+    Private Sub BtnDividir_Click(sender As Object, e As EventArgs) Handles btnDividir.Click
+        If txtPainel.Text = "" Then
+            MessageBox.Show("Ação inválida, insira um número!")
+        Else
+            num1 = txtPainel.Text
+            operador = "/"
+            txtPainel.Text = ""
+        End If
+    End Sub
+    Private Sub BtnPorcentagem_Click(sender As Object, e As EventArgs) Handles btnPorcentagem.Click
+        If txtPainel.Text = "" Then
+            MessageBox.Show("Ação inválida, insira um número!")
+        Else
+            num1 = txtPainel.Text
+            operador = "%"
+            txtPainel.Text = ""
+        End If
+    End Sub
+
+    Private Sub BtnResultado_Click(sender As Object, e As EventArgs) Handles btnResultado.Click
+        If txtPainel.Text = "" Then
+            MessageBox.Show("Ação inválida, insira um número!")
+        Else
+            num2 = txtPainel.Text
+            If operador = "x" Then
+                txtPainel.Text = num1 * num2
+            End If
+            If operador = "+" Then
+                txtPainel.Text = num1 + num2
+            End If
+            If operador = "-" Then
+                txtPainel.Text = num1 - num2
+            End If
+            If operador = "/" Then
+                txtPainel.Text = num1 / num2
+            End If
+            If operador = "%" Then
+                txtPainel.Text = (num1 / 100) * num2
+            End If
+        End If
+    End Sub
+    Private Sub BtnZero_Click(sender As Object, e As EventArgs) Handles btnZero.Click
+        txtPainel.Text += "0"
+    End Sub
+    Private Sub BtnUm_Click(sender As Object, e As EventArgs) Handles btnUm.Click
+        txtPainel.Text += "1"
+    End Sub
+    Private Sub BtnDois_Click(sender As Object, e As EventArgs) Handles btnDois.Click
+        txtPainel.Text += "2"
+    End Sub
+    Private Sub BtnTres_Click(sender As Object, e As EventArgs) Handles btnTres.Click
+        txtPainel.Text += "3"
+    End Sub
+    Private Sub BtnQuatro_Click(sender As Object, e As EventArgs) Handles btnQuatro.Click
+        txtPainel.Text += "4"
+    End Sub
+    Private Sub BtnCinco_Click(sender As Object, e As EventArgs) Handles btnCinco.Click
+        txtPainel.Text += "5"
+    End Sub
+    Private Sub BtnSeis_Click(sender As Object, e As EventArgs) Handles btnSeis.Click
+        txtPainel.Text += "6"
+    End Sub
+    Private Sub BtnSete_Click(sender As Object, e As EventArgs) Handles btnSete.Click
+        txtPainel.Text += "7"
+    End Sub
+    Private Sub BtnOito_Click(sender As Object, e As EventArgs) Handles btnOito.Click
+        txtPainel.Text += "8"
+    End Sub
+    Private Sub BtnNove_Click(sender As Object, e As EventArgs) Handles btnNove.Click
+        txtPainel.Text += "9"
+    End Sub
+    Private Sub BtnVirgula_Click(sender As Object, e As EventArgs) Handles btnVirgula.Click
+        txtPainel.Text += ","
+    End Sub
+    Private Sub BtnLimpar_Click(sender As Object, e As EventArgs) Handles btnLimpar.Click
+        txtPainel.Text = ""
+    End Sub
+    Private Sub BtnBackspace_Click(sender As Object, e As EventArgs) Handles btnBackspace.Click
+        txtPainel.Text = txtPainel.Text.Substring(0, txtPainel.Text.Length - 1)
+    End Sub
+
+
+End Class
